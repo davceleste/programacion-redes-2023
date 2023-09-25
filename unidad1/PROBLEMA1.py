a06dca9eb8e05c5e4564163d69ecdad9224dbd78
davceleste <142541770+davceleste@users.noreply.github.com>
2023-09-25 11:56:28 -0600

    Add files via upload

---
+++ unidad1/PROBLEMA1.py
@@ -0,0 +1,7 @@
+﻿precio_normal = 3.49
+descuento = 60  # 60%
+barras_no_frescas = int(input("Ingrese el número de barras no frescas vendidas: "))
+costo_total = barras_no_frescas * precio_normal * (1 - descuento / 100)
+print("Precio habitual de una barra de pan: ${precio_normal:.2}")
+print("Descuento por no ser fresca ({descuento}%): ${precio_normal * descuento / 100:.2}")
+print("Costo final total: ${costo_total:.2}")
